96b8f80a34b5d05558441b5ecf2a9928ef997cf0
Helmut Sey <Helmutsey@gmail.com>
2025-10-20 01:01:59 +0000

    text update in page

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Container>
       <h1 className="text-3xl font-bold">BloodyARK WebApp</h1>
-      <p className="mt-2 text-gray-600">Next.js + Tailwind läuft ✅</p>
+      <p className="mt-2 text-gray-600">Next.js + Tailwind läuft gut✅</p>
     </Container>
   );
 }
